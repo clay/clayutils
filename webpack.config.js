3840dbdaebed1f6e9d73674a35cab1b6892bb13b
Micah Allen <micah.allen@swarmsolutions.com>
2019-10-07 12:41:11 -0400

    Add build step (#27)
    * remove destructuring usages
    
    * Revert "remove destructuring usages"
    
    * Add build step
    
    * Simplify babel targets

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  target: 'web',
+  entry: './index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
